0e0664ad8e2602d985591c4323b112afe6560b9c
Caspar Clemens Mierau <ccm@c-base.org>
2012-08-14 16:12:32 +0200

    fixing path issue

--- manifests/system.pp
+++ manifests/system.pp
@@ -10,4 +10,55 @@ class rvm::system($version='latest') {
       Class['rvm::dependencies'],
     ],
   }
+  file {
+    '/usr/local/bin/rvm':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/bundle':
+      ensure => link,
+      target => '/usr/local/rvm/bin/bundle';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rake':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rake';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm-auto-ruby':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm-auto-ruby';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm-exec':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm-exec';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm-installer':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm-installer';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm-prompt':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm-prompt';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm-shell':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm-shell';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvm-smile':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvm-smile';
+      require => Exec['system-rvm'],
+    '/usr/local/bin/rvmsudo':
+      ensure => link,
+      target => '/usr/local/rvm/bin/rvmsudo';
+      require => Exec['system-rvm'],
+    }
 }
+
+
+
+
+
